6e89360967ca08e1d679665f11f601a5b5b0be85
Maxime Gagnon-Legault <maxime.glegault@gmail.com>
2017-03-23 15:28:34 -0400

    1 et 2 fini

--- Challenge 1 and 2/challenge_1.py
+++ Challenge 1 and 2/challenge_1.py
@@ -8,8 +8,12 @@ POKEDEX = "pokemons_list.csv"
 if __name__ == "__main__":
     pokedex = None
     list_of_name = {}
+    list_of_pokemon = {}
+
     pokedex = open(POKEDEX, "r")
+    # skip first line
     next(pokedex)
+    # get name from arguments
     scrambled_name_of_pokemon = sys.argv[1].lower()
     ascii_sum_of_scrambled = sum(ord(letter) for letter in scrambled_name_of_pokemon)
 
@@ -20,29 +24,23 @@ if __name__ == "__main__":
             list_of_name[ascii_sum] = [a[1]]
         else:
             list_of_name[ascii_sum].append(a[1])
-    print(list_of_name)
+        list_of_pokemon[a[1]] = (a[0], a[1], a[2])
 
     temp_scrambled_name = scrambled_name_of_pokemon
-    possible_name = list_of_name[ascii_sum_of_scrambled].copy()
-    for pokemon in list_of_name[ascii_sum_of_scrambled]:
-        print(possible_name)
-
-        print(pokemon)
+    possible_name = list_of_name.get(ascii_sum_of_scrambled, []).copy()
+    for pokemon in list_of_name.get(ascii_sum_of_scrambled, []):
         for letter in pokemon:
-            print(letter)
             result = temp_scrambled_name.find(letter.lower())
             if result != -1:
                 temp_scrambled_name = temp_scrambled_name[:result] + temp_scrambled_name[result + 1:]
             else:
-                possible_name.remove(pokemon)
+                possible_name.pop(0)
                 break
         temp_scrambled_name = scrambled_name_of_pokemon
 
-
-
-
-
     if possible_name:
-        print(possible_name)
+        print(list_of_pokemon[possible_name[0]][1], ": His number is ",
+              list_of_pokemon[possible_name[0]][0], " and he is of type",
+              list_of_pokemon[possible_name[0]][2].lower(), ".")
     else:
         print("Pokemon not found!")
